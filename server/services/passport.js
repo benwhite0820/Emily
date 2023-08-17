9bd69d37d84cf95be68fd9a13c33224f88b58086
BenTsai <ben.tsai@kangdainfo.com>
2023-08-17 11:32:50 +0800

    add additional proxy option for google strategy

--- server/services/passport.js
+++ server/services/passport.js
@@ -24,6 +24,7 @@ passport.use(
             clientID: googleClientID,
             clientSecret: googleClientSecret,
             callbackURL: '/auth/google/callback',
+            proxy: true,
         },
         async (accessToken, refreshToken, profile, done) => {
             const existingUser = await User.findOne({ googleId: profile.id });
